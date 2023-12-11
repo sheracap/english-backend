2d3edcaa0a1274c56ca15100ce6a88a776587fd9
Sherzod Tursunov <shtursunov@uzkassa.uz>
2023-12-11 16:21:09 +0500

    notifications start

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const fileUpload = require("express-fileupload");
 const router = require("./server/routes/index");
 const errorHandler = require("./server/middleware/ErrorHandlingMiddleware");
 const path = require("path");
+const events = require("events");
 
 const PORT = process.env.PORT || 5000;
+const emitter = new events.EventEmitter();
 
 const app = express();
 app.use(cors());
@@ -23,6 +25,26 @@ app.use(errorHandler);
 app.get("/", (req, res) => {
   res.status(200).json({ message: "working" });
 });
+
+app.get("/connect", (req, res) => {
+  res.writeHead(200, {
+    "Connection": "keep-alive",
+    "Content-Type": "text/event-stream",
+    "Cache-Control": "no-cache"
+  });
+
+  emitter.on("newMessage", (message) => {
+    if (String(message.id) === String(req.query.userid)) {
+      res.write(`data: ${JSON.stringify(message)} \n\n`);
+    }
+  });
+});
+
+app.post("/new-message", ((req, res) => {
+  const message = req.body;
+  emitter.emit("newMessage", message);
+  res.status(200).send("hey");
+}));
 
 const start = async () => {
   try {
